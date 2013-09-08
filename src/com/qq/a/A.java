1c902573c2f9a330630c77b560aa7cea1855156e
Kiki <Kiki@Kiki-PC>
2013-09-08 23:00:23 +0700

    second commit - add main method in class A and add new output in DemoLocalGit

--- src/com/qq/a/A.java
+++ src/com/qq/a/A.java
@@ -9,5 +9,8 @@ package com.qq.a;
  * @author Kiki
  */
 public class A {
-    
+
+    public static void main(String[] args) {
+        System.out.println("A1");
+    }
 }
--- src/demolocalgit/DemoLocalGit.java
+++ src/demolocalgit/DemoLocalGit.java
@@ -16,5 +16,6 @@ public class DemoLocalGit {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("test 1");
+        System.out.println("test 2");
     }
 }
