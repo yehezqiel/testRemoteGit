91a6fc59d485f734493853ec9cc24a2ad52e8adf
Kiki <Kiki@Kiki-PC>
2013-09-08 22:51:09 +0700

    first commit - begin program

---
+++ src/demolocalgit/DemoLocalGit.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demolocalgit;
+
+/**
+ *
+ * @author Kiki
+ */
+public class DemoLocalGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("test 1");
+    }
+}
