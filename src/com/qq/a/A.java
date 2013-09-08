99b1d40ff2ef0340ef3d0792b5d3df514fb89690
Kiki <Kiki@Kiki-PC>
2013-09-08 23:19:50 +0700

    Revert commit

--- src/com/qq/a/A.java
+++ src/com/qq/a/A.java
@@ -9,8 +9,5 @@ package com.qq.a;
  * @author Kiki
  */
 public class A {
-
-    public static void main(String[] args) {
-        System.out.println("A1");
-    }
+    
 }
--- src/demolocalgit/DemoLocalGit.java
+++ src/demolocalgit/DemoLocalGit.java
@@ -16,6 +16,5 @@ public class DemoLocalGit {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("test 1");
-        System.out.println("test 2");
     }
 }
